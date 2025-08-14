6749920bb51e0b335cb17dc46aa55de4b6ecdc87
Christian Charbonnier <christian.charbonnier@estudiantes.utec.edu.uy>
2025-08-14 13:37:46 -0300

    validacion

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,31 @@
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+  document.getElementById("alert-success").classList.add("show");
+  
 }
 
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
+
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("submit", function(event) {
+        
+        const nombre = document.getElementById("nombre").value.trim();
+        const apellido = document.getElementById("apellido").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const password1 = document.getElementById("password1").value;
+        const password2 = document.getElementById("password2").value;
+        const terminos = document.getElementById("terminos").checked;
+
+        if (nombre && apellido && email && password1 && password2 && terminos) {
+            if (password1 === password2) {
+                showAlertSuccess();
+            } else {
+                showAlertError();
+            }
+        } else {
+            showAlertError();
+        }
+    });
+});
